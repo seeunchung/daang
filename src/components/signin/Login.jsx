deaabf00a235add466ce292469d9b46894bfffcc
DESKTOP-SMD51TC\kiik5 <kiik52@naver.com>
2023-10-30 18:06:19 +0900

    메인페이지 sec1,3 구현

--- src/components/signin/Login.jsx
+++ src/components/signin/Login.jsx
@@ -13,21 +13,19 @@ export default function Login() {
             <h2>로그인</h2>
           </div>
           <div className="content">
-            <form action="/resopon.php">
-              <ul>
+            <form action="/resopon.php" className='login-form'>
+              <ul className='custom-login'>
                 <li><input type="text" id='id' name='id' placeholder='아이디를 입력해주세요.' /></li>
                 <li><input type="password" id='pw' name='pw' placeholder='비밀번호를 입력해주세요' /></li>
                 <li><a href="#!">아이디 찾기</a><i>|</i><a href="#!">비밀번호 찾기</a></li>
                 <li><button type='submit' className='submit-btn'><span className='login-text'>로그인</span></button></li>
                 <li><Link to='/signup'><button type='button' className='signup-btn'><span className='signup-text'>회원가입</span></button></Link></li>
-                <li>
-                  <ul className='sns-login'>
-                    <li><a href="#!"><img src="./img/kakao_logo.png" alt="" /></a></li>
-                    <li><a href="#!"><img src="./img/naver_logo.png" alt="" /></a></li>
-                    <li><a href="#!"><img src="./img/google_logo.png" alt="" /></a></li>
-                  </ul>
-                </li>
               </ul>
+              <div className='sns-login'>
+                <button type='button'><img src="./img/naver_logo.png" alt="" /></button>
+                <button type='button'><img src="./img/kakao_logo.png" alt="" /></button>
+                <button type='button'><img src="./img/google_logo.png" alt="" /></button>
+              </div>
             </form>
           </div>
         </div>
