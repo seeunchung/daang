56305a2e59cffded4125b478e2f8b151694e42c8
Gavin Choi <laserc729@gmail.com>
2023-10-31 14:56:41 +0900

    gavin

--- src/components/dsta/DstaDetail.jsx
+++ src/components/dsta/DstaDetail.jsx
@@ -1,20 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import App from './dsta';
-
-export default function DstaMainPage() {
-  return (
-    <main id='main'>
-      <div className="container">
-        <h2>d스타</h2>
-        <App />
-      </div>
-    </main>
-  );  
-};
-
-
-=======
 
 export default function DstaDetail() {
   return (
@@ -23,4 +7,3 @@ export default function DstaDetail() {
     </div>
   );
 };
->>>>>>> 1d28f4ada11067d4174a99ea1ad6547a860f66d4
