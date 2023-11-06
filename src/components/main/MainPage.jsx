debe171d715e817d83ec145a8e53beb9b3e8502a
d0ngri <dongyeop1030@gmail.com>
2023-11-06 09:53:38 +0900

    no message

--- src/components/main/MainPage.jsx
+++ src/components/main/MainPage.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 
 export default function MainPage() {
   return (
-    <main id='main'>
-      <div className="container">
-        <h2>메인</h2>
-        
-        <h2>메인</h2>
-      </div>
-    </main>
-  );
-};
+    <main id="main"></main>
+      )
+     }
