e91f8048798089ca81af7e5b62bf17b8875a923a
seeunchung <jjssee210@gmail.com>
2023-11-07 16:32:40 +0900

    마이페이지메인 스타일 조정

--- src/components/mypage/MyPageMainPage.jsx
+++ src/components/mypage/MyPageMainPage.jsx
@@ -63,142 +63,142 @@ export default function MyPageMainPage() {
     setCurrentPage(1) //페이지네이션 상태 초기화
   }
 
-
-
   return (
     <main id='mypage' className='mypage_main'>
-      {/*학생증 섹션 */}
-      <section className='card_section'>
-        <div className='mypagemain_titlecontainer'>
-          <img src="./img/mypage/mypage_dog.png" alt="강아지 아이콘" />
-          <h2 className='mypagemain_title'>My Page</h2>
-        </div>
-        <img className='page_prev' src='./img/mypage/idcard_page.png' alt='<버튼'></img>
-        <img className='page_next' src='./img/mypage/idcard_page.png' alt='>버튼'></img>
-        <div className='mypageedit_idcard'>
-          <div className='mypageedit_idcardhead'>
-            <div className='mypageedit_headcontainer'>
-              <img src="./img/mypage/idcard_dog.png" alt="강아지 메인 학생증 선택 아이콘" />
-              <h2 className='mypageedit_headtitle'>STUDENT</h2>
-            </div>
+      <div className='mypage_main_container'>
+        {/*학생증 섹션 */}
+        <section className='card_section'>
+          <div className='mypagemain_titlecontainer'>
+            <img src="./img/mypage/mypage_dog.png" alt="강아지 아이콘" />
+            <h2 className='mypagemain_title'>My Page</h2>
           </div>
-          <img className='idcard_stamp' src='./img/mypage/idcard_stamp.png' alt="학생증 도장" />
-          <div className='idcard_bodycontainer'>
-            <img className='idcard_dog' src='./img/mypage/dog_img.png' alt='강아지 사진' />
+          <img className='page_prev' src='./img/mypage/idcard_page.png' alt='<버튼'></img>
+          <img className='page_next' src='./img/mypage/idcard_page.png' alt='>버튼'></img>
+          <div className='mypageedit_idcard'>
+            <div className='mypageedit_idcardhead'>
+              <div className='mypageedit_headcontainer'>
+                <img src="./img/mypage/idcard_dog.png" alt="강아지 메인 학생증 선택 아이콘" />
+                <h2 className='mypageedit_headtitle'>STUDENT</h2>
+              </div>
+            </div>
+            <img className='idcard_stamp' src='./img/mypage/idcard_stamp.png' alt="학생증 도장" />
+            <div className='idcard_bodycontainer'>
+              <img className='idcard_dog' src='./img/mypage/dog_img.png' alt='강아지 사진' />
 
-            <div className='idcard_info'>
-              <div>
-                <h2 className='idcard_infotext'>이름 :</h2>
-                <div className='idcard_infobox'>
-                  <div className='info_textarea'>
-                    {idcardInfo.length > 0 && idcardInfo[0].name}
+              <div className='idcard_info'>
+                <div>
+                  <h2 className='idcard_infotext'>이름 :</h2>
+                  <div className='idcard_infobox'>
+                    <div className='info_textarea'>
+                      {idcardInfo.length > 0 && idcardInfo[0].name}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h2 className='idcard_infotext'>학과 :</h2>
+                  <div className='idcard_infobox'>
+                    <div className='info_textarea'>
+                      {idcardInfo.length > 0 && idcardInfo[0].breed}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h2 className='idcard_infotext'>학번 :</h2>
+                  <div className='idcard_infobox'>
+                    <div className='info_textarea'>
+                      {idcardInfo.length > 0 && idcardInfo[0].birth}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h2 className='idcard_infotext'>특징 :</h2>
+                  <div className='idcard_infobox'>
+                    <div className='info_textarea'>
+                      {idcardInfo.length > 0 && idcardInfo[0].character}
+                    </div>
                   </div>
                 </div>
               </div>
-              <div>
-                <h2 className='idcard_infotext'>학과 :</h2>
-                <div className='idcard_infobox'>
-                  <div className='info_textarea'>
-                    {idcardInfo.length > 0 && idcardInfo[0].breed}
-                  </div>
-                </div>
-              </div>
-              <div>
-                <h2 className='idcard_infotext'>학번 :</h2>
-                <div className='idcard_infobox'>
-                  <div className='info_textarea'>
-                    {idcardInfo.length > 0 && idcardInfo[0].birth}
-                  </div>
-                </div>
-              </div>
-              <div>
-                <h2 className='idcard_infotext'>특징 :</h2>
-                <div className='idcard_infobox'>
-                  <div className='info_textarea'>
-                    {idcardInfo.length > 0 && idcardInfo[0].character}
-                  </div>
-                </div>
-              </div>
+              <div className='idcard_univ'>DAaaNG UNIV</div>
+              <img className='idcard_barcode' src='./img/mypage/barcode.png' alt='바코드 사진' />
             </div>
-            <div className='idcard_univ'>DAaaNG UNIV</div>
-            <img className='idcard_barcode' src='./img/mypage/barcode.png' alt='바코드 사진' />
           </div>
-        </div>
-        <div className='btn_box'>
-          <Link to='/mypage-edit'> <button className='edit_btn' type='submit'>회원정보 수정</button> </Link>
-        </div>
-      </section>
-      {/*내 글 모아보기 */}
-      <section className='myposts'>
-        <ul className='list_btn_box'>
-          {buttonTabs.map((tab, index) => (
-            <li className='tabs' key={index}>
-              <button
-                onClick={() => handleButtonClick(index)}
-                className={`tab_btn ${activeButton === index ? 'active' : ''}`}
-              >
-                {tab}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <div className='myposts_container'>
-          {activeTab === 0 && (
-            <div className='total_box'>
-              <select className="post_filter" onChange={handleSelectChange} value={selectedOption}>
-                <option value="dmunity">댕뮤니티</option>
-                <option value="dsta">댕스타</option>
-              </select>
-              {selectedOption === "dmunity" && (
-                <div className='list_box'>
-                  <table className='list_table'>
-                    <thead>
-                      <tr className='head_row'>
-                        <th>번호</th>
-                        <th>제목</th>
-                        <th>작성일</th>
-                        <th>조회수</th>
-                      </tr>
-                    </thead>
-                    <tbody className='body_row'>
-                      {getCurrentPagePosts(10, myDmunity).map((post) => (
-                        <tr key={post.postid} className="data_row">
-                          <td>{post.number}</td>
-                          <td>{post.title}</td>
-                          <td>{post.date}</td>
-                          <td>{post.view}</td>
+          <div className='btn_box'>
+            <Link to='/mypage-edit'> <button className='edit_btn' type='submit'>회원정보 수정</button> </Link>
+          </div>
+        </section>
+        {/*내 글 모아보기 */}
+        <section className='myposts'>
+          <ul className='list_btn_box'>
+            {buttonTabs.map((tab, index) => (
+              <li className='tabs' key={index}>
+                <button
+                  onClick={() => handleButtonClick(index)}
+                  className={`tab_btn ${activeButton === index ? 'active' : ''}`}
+                >
+                  {tab}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className='myposts_container'>
+            {activeTab === 0 && (
+              <div className='total_box'>
+                <select className="post_filter" onChange={handleSelectChange} value={selectedOption}>
+                  <option value="dmunity">댕뮤니티</option>
+                  <option value="dsta">댕스타</option>
+                </select>
+                {selectedOption === "dmunity" && (
+                  <div className='list_box'>
+                    <table className='list_table'>
+                      <thead>
+                        <tr className='head_row'>
+                          <th>번호</th>
+                          <th>제목</th>
+                          <th>작성일</th>
+                          <th>조회수</th>
                         </tr>
+                      </thead>
+                      <tbody className='body_row'>
+                        {getCurrentPagePosts(10, myDmunity).map((post) => (
+                          <tr key={post.postid} className="data_row">
+                            <td>{post.number}</td>
+                            <td>{post.title}</td>
+                            <td>{post.date}</td>
+                            <td>{post.view}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                    <Pagination
+                      currentPage={currentPage}
+                      totalPages={Math.ceil(myDmunity.length / 10)}
+                      onPageChange={handlePageChange}>
+                    </Pagination>
+                  </div>
+                )}
+                {selectedOption === 'dsta' && (
+                  <div className='dsta_listbox'>
+                    <ul className='dsta_lists'>
+                      {getCurrentPagePosts(8, myDsta).map((post) => (
+                        <li key={post.postid}>
+                          <img className='dsta_list' src={post.img} alt='thumbnail_img' />
+                        </li>
                       ))}
-                    </tbody>
-                  </table>
-                  <Pagination
-                    currentPage={currentPage}
-                    totalPages={Math.ceil(myDmunity.length / 10)}
-                    onPageChange={handlePageChange}>
-                  </Pagination>
-                </div>
-              )}
-              {selectedOption === 'dsta' && (
-                <div className='dsta_listbox'>
-                  <ul className='dsta_lists'>
-                    {getCurrentPagePosts(8, myDsta).map((post) => (
-                      <li key={post.postid}>
-                        <img className='dsta_list' src={post.img} alt='thumbnail_img' />
-                      </li>
-                    ))}
-                  </ul>
-                  <Pagination
-                    currentPage={currentPage}
-                    totalPages={Math.ceil(myDsta.length / 8)}
-                    onPageChange={handlePageChange}>
-                  </Pagination>
-                </div>
-              )}
-            </div>
-          )}
+                    </ul>
+                    <Pagination
+                      currentPage={currentPage}
+                      totalPages={Math.ceil(myDsta.length / 8)}
+                      onPageChange={handlePageChange}>
+                    </Pagination>
+                  </div>
+                )}
+              </div>
+            )}
 
-        </div>
-      </section>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
